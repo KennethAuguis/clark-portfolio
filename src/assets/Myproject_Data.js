a37cfefa76ab6aab156fef045825dfda1ff8ef42
KennethAuguis <141295321+KennethAuguis@users.noreply.github.com>
2025-08-06 21:03:12 +0800

    Update portfolio

--- src/assets/Myproject_Data.js
+++ src/assets/Myproject_Data.js
@@ -1,5 +1,7 @@
 import Tatay from "../assets/Tatay.png";
 import KIM from "../assets/KIM.png";
+import Brand_Stylescape from "../assets/Brand_Stylescape.png";
+import Bento from "../assets/Bento.png";
 
 const Myproject_Data = [
   {
@@ -12,7 +14,19 @@ const Myproject_Data = [
     w_no: 2,
     w_name: "Tatay_img",
     w_img: Tatay,
-    w_link: "" // Optional: add a different link if available
+    w_link: "https://www.figma.com/proto/T0IS4at1hXLQQTr472raU1/E.N.A?node-id=1-2&p=f&t=9FQCJwcYhiSlpSmj-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2" // Optional: add a different link if available
+  },
+    {
+    w_no: 3,
+    w_name: "Brand_Stylescape_img",
+    w_img: Brand_Stylescape,
+    w_link: "https://www.figma.com/proto/uZMWcMK3KGGDBQV3C76YqP/M-R?node-id=114-286&t=pO9yb0PXmWYqGq84-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=9%3A2&show-proto-sidebar=1" // Optional: add a different link if available
+  },
+      {
+    w_no: 4,
+    w_name: "Bento_img",
+    w_img: Bento,
+    w_link: "https://www.figma.com/proto/uZMWcMK3KGGDBQV3C76YqP/M-R?node-id=90-171&t=sROlS4zjYo0romEO-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=9%3A2&show-proto-sidebar=1" // Optional: add a different link if available
   }
 ];
 
