49c70f578e199c8be35cf980272ad1003c9f72e9
Kenneth Auguis <kcgauguis.student@ua.edu.ph>
2026-03-26 13:01:23 +0800

    latest website update

--- src/assets/Myproject_Data.js
+++ src/assets/Myproject_Data.js
@@ -1,8 +1,7 @@
 import Tatay from "../assets/Tatay.png";
 import Vendor_Dashboard from "../assets/Vendor_Dashboard.png";
-import Brand_Stylescape from "../assets/Brand_Stylescape.png";
 import Ordering_System from "../assets/Ordering_System.png";
-import Bento from "../assets/Bento.png";
+import Rider_app from "../assets/Rider_app.png";
 
 const Myproject_Data = [
   {
@@ -19,15 +18,9 @@ const Myproject_Data = [
   },
     {
     w_no: 3,
-    w_name: "Brand_Stylescape_img",
-    w_img: Brand_Stylescape,
-    w_link: "https://www.figma.com/proto/uZMWcMK3KGGDBQV3C76YqP/M-R?node-id=114-286&t=pO9yb0PXmWYqGq84-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=9%3A2&show-proto-sidebar=1" // Optional: add a different link if available
-  },
-      {
-    w_no: 4,
-    w_name: "Bento_img",
-    w_img: Bento,
-    w_link: "https://www.figma.com/proto/uZMWcMK3KGGDBQV3C76YqP/M-R?node-id=90-171&t=sROlS4zjYo0romEO-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=9%3A2&show-proto-sidebar=1" // Optional: add a different link if available
+    w_name: "Rider_app",
+    w_img: Rider_app,
+    w_link: "https://apporderingsystem.my.canva.site/rider-app-syetem" 
   }
 ];
 
